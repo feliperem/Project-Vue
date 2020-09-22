8ef1846b480c42e1fbb73b2aae016c6c7bbb964e
feliperem <felipe.eufrasio@hotmail.com>
2020-09-22 19:23:11 -0300

    Alteração no dashboard Titulo

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/Felipe.jpg">
-    <HelloWorld msg="O Felipe é muito muito bom"/>
+    <HelloWorld msg="O Felipe é muito bom"/>
   </div>
 </template>
 
